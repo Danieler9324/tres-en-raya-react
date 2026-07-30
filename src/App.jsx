e75e4daad9442691788034eb33e78e40ac9c7050
Danieler9324 <danielviramontes562@gmail.com>
2026-07-30 14:38:28 -0600

    feat: Se agrego boton para alternar el orden de la lista de movimientos

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {useState} from 'react';
 export default function Game () {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [isAscending,setIsAscending] = useState(true);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -18,18 +19,24 @@ export default function Game () {
 
   const moves = history.map((squares, move) => {
     let content;
+    let order;
     if (move === currentMove) {
       content = move > 0 ? "Estas en el movimiento #" + move : "Estas en el inicio del juego";
     } else {
       const description = move > 0 ? "Ir al movimiento #" + move : "Ir al inicio del juego";
       content = <button onClick={() => jumpTo(move)}>{description}</button>
     }
+
     return (
-      <li key={move}>
-        {content}
-      </li>
+        <li key={move}>
+          {content}
+        </li>
     );
   });
+
+  if(!isAscending){
+    moves.reverse();
+  }
 
   return (
   <div className="game">
@@ -37,6 +44,8 @@ export default function Game () {
       <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
     </div>
     <div className="game-info">
+      <button onClick={() => setIsAscending(!isAscending)}>{isAscending ? "Ordenar descendente" : "Ordenar Ascendente"}</button>
+
       <ol>{moves}</ol>
     </div>
   </div>
@@ -65,26 +74,30 @@ function Board({ xIsNext, squares, onPlay }) {
     status = 'Siguiente Jugador: ' + (xIsNext ? 'X' : 'O');
   }
 
+  const boardRows = [];
+  for (let row = 0; row < 3; row++){
+    const rowSquares = [];
+    for (let col = 0; col <3; col++){
+      const squareIndex = row * 3 + col;
+      rowSquares.push(
+        <Square
+        key={squareIndex}
+        value={squares[squareIndex]}
+        onSquareClick={() => handleClick(squareIndex)}
+        />
+      );
+    }
+    boardRows.push(
+      <div key={row} className='board-row'>
+        {rowSquares}
+      </div>
+    )
+  }
+
   return (
   <div>
     <div className="status">{status}</div>
-    <div className="board-row">
-      <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-      <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-      <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-    </div>
-
-    <div className="board-row">
-      <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-      <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-      <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-    </div>
-
-    <div className="board-row">
-      <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-      <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-      <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-    </div>
+    {boardRows}
   </div>
   )
 }
